7765317bc1a9cc3fe4dabb46d6b6801ae8ee0028
akshay <akshay.tabib@hicare.in>
2023-09-12 16:47:15 +0530

    Login UI done. login_screen.dart

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -8,37 +8,35 @@ import 'Header.dart';
 import 'InputWrapper.dart';
 import 'package:provider/provider.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var splashscreenimg = 'images/splash.png';
 
     // TODO: implement build
     return Scaffold(
       body: Container(
-      width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(begin: Alignment.topCenter,colors: [
-           Colors.green,
-           Colors.green,
-           Colors.green
-          ]),
-        ),
-
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 80,),
-            Header(),
-            Expanded(child: Container(
+            Image.asset(splashscreenimg, height: 130, width: 130),
+            SizedBox(height: 10),
+            Text("India's Hygiene Expert",
+                style: const TextStyle(color: Colors.black, fontSize: 20)),
+            SizedBox(height: 10),
+            Text("30+ years of legacy",
+                style: const TextStyle(color: Colors.black, fontSize: 20)),
+            Expanded(
+                child: Container(
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(60),
-                  topRight: Radius.circular(60),
-                )
-              ),
-
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  )),
               child: InputWrapper(),
-
             ))
           ],
         ),
@@ -46,3 +44,35 @@ class LoginPage extends StatelessWidget{
     );
   }
 }
+
+// class LoginPage extends StatefulWidget {
+//   @override
+//   State<LoginPage> createState() {
+//     // TODO: implement createState
+//     return _LoginPage();
+//   }
+// }
+//
+// class _LoginPage extends State<LoginPage> {
+//   var splashscreenimg = 'images/splash.png';
+//
+//   @override
+//   Widget build(Object context) {
+//     // TODO: implement build
+//     return  Column(
+//       children: <Widget>[
+//         SizedBox(height: 60),
+//         Column(
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           children: [
+//             Image.asset(splashscreenimg, height: 50, width: 50),
+//           ],
+//         ),
+//         SizedBox(height: 10),
+//         Text("India's Hygiene Expert",style: const TextStyle(color: Colors.amberAccent,fontSize: 13)),
+//         SizedBox(height: 10),
+//         Text("30+ years of legacy",style: const TextStyle(color: Colors.amberAccent,fontSize: 12)),
+//       ],
+//     );
+//   }
+// }
